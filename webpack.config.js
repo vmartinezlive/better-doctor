17f91cfc92ce09b19fd6804fd3c06aea411c6ee2
Doris Warren and Matt Groberg <matt@>
2019-03-13 11:40:58 -0700

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,17 @@ module.exports= {
           /spec/
         ],
         loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: ['es2015']
+        }
       }
     ]
   }
