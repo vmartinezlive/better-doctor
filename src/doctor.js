dd9ed74af483955d970c219c39a74fa97aef5388
Student <example@example.com>
2019-03-26 19:29:28 -0700

    add loop for name, address, medicine and bio to ui.

---
+++ src/doctor.js
@@ -0,0 +1,15 @@
+// export class Doctor {
+//   construcotr(name, address) {
+//     this.name = name;
+//     this.address = address;
+//   }
+//
+//   for(i = 0; i < doctor.length; i ++) {
+//     if doctor
+//   }
+//
+// }
+//
+//
+// let name = (`${body.data[0].profile.first_name + " " + body.data[0].profile.last_name}`);
+// let address = (`${body.data[0].practices[0].visit_address.city + " " + body.data[0].practices[0].visit_address.street + " " + body.data[0].practices[0].visit_address.state + " " + body.data[0].practices[0].visit_address.zip}`);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,37 +11,56 @@ $(document).ready(function() {
     event.preventDefault();
     const symptoms = $('#inputSymptoms').val();
     const doctor = $('#inputDoctor').val();
-    // console.log(doctor);
+    // console.log("I'm here");
 
-//
-//     // const nameInput = $("#inputName").val();
-//     // const addressInput = $("#inputAddress").val();
-//     // const cityInput = $("#inputCity").val();
-//     // const stateInput = $("#inputState").val();
-//     // const zipInput = $("#inputZip").val();
-//     let symptomsInput = $("#inputSymptoms").val();
-//     let doctorInput = $("#inputDoctor").val();
-//
+
     let newDoctorSearch = new DoctorSearch();
     let promise = newDoctorSearch.getDoctor(symptoms, doctor)
 
 
     promise.then(function(response) {
     let body =JSON.parse(response);
-    console.log(body);
-    let first_name = (`${body.data[3].profile.first_name}`);
-    let last_name = (`${body.data[3].profile.last_name}`);
-    let city = (`${body.data[3].practices[0].visit_address.city}`);
-    let street = (`${body.data[3].practices[0].visit_address.street}`);
-    let state = (`${body.data[3].practices[0].visit_address.state}`);
-    let zip = (`${body.data[3].practices[0].visit_address.zip}`);
+    // console.log(body);
+
+    let name = [];
+    let address = [];
+    let medicine = [];
+    let bio = [];
+    const nameHTML = [];
+
+    var i = 0;
+    for(i in body.data) {
+      name = body.data[i].profile.first_name + " " + body.data[i].profile.last_name;
+
+      address = body.data[i].practices[0].visit_address.city + " " + body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.state_long + " " + body.data[i].practices[0].visit_address.zip + " " + "Phone: " +
+      body.data[i].practices[0].phones[0].number;
+
+      medicine = body.data[i].specialties[0].name ;
+      bio = body.data[i].profile.bio;
+
+      $("#doctorsList").append("<li>" + name + "<br>" + address + "<br>" + medicine +  "<br>" + bio +"</li>");
 
 
-    const name = first_name + last_name + city + street + state + zip;
+
+
+
+
+      console.log(address);
+
+    nameHTML.push(name + address );
+
+
+
+    }
+
+
+
+
+    // const name = firstName;
     // console.log(body.meta.data.practices[]);
 
-    $('#results1').text(name);
-    console.log('results1');
+    // $('#results1').text(name);
+    // console.log('results1');
     $('#results2').text(`${doctor}`);
 }, function(error) {
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
